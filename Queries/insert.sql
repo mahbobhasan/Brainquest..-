71f88e4f57bcbdfab3ea507582b94d2934533731
Mahbob Hasan Sazib <117675277+mahbobhasan@users.noreply.github.com>
2024-10-16 22:29:00 +0800

    indentation corrected

--- Queries/insert.sql
+++ Queries/insert.sql
@@ -2,8 +2,8 @@ use department;
 
 insert into role
 (id,role_name) values(1,"student"),
-					(2,"teacher"),
-                    (3,"admin");
+		     (2,"teacher"),
+                     (3,"admin");
 
 insert into users
 (id,name,username,email,image,role_id,password) values(1,"DR. Asadun Nobi","asad_00","asadunnobi@gmail.com","hello",3,"password");
