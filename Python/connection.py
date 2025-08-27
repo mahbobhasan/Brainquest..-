2fdd95470e7a8961e57c396ac5c28b6223d5c64a
mahbobhasan <mahbobhasan66@gmail.com>
2025-08-27 23:11:28 +0600

    changed the mysql-connector-python version segmentation fault solved

--- Python/connection.py
+++ Python/connection.py
@@ -3,21 +3,20 @@ class DB_Connector:
     status=False
     def __init__(self):
         try:
-            print('hello')
             self.connection=connector.connect(
-            host='127.0.0.1',
+            host='localhost',
+            port='3306',
             user='root',
             password='',
             database='department'
             )
-            print("hello")
             self.cursor=self.connection.cursor(dictionary=True)
             self.status=True
         except Exception as e:
             self.status=False
             print(self.status)
             print(e)
-
 conn=DB_Connector()
+print(conn.status)
     
     
